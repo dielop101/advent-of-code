b2b8dd6eabd46495a3d995966304bf919132cc67
Diego López <diegolopezperez91@gmail.com>
2023-12-01 18:51:58 +0100

    Refactor structures

---
+++ 2022/Day12/Logic.cs
@@ -0,0 +1,118 @@
+﻿using Helpers;
+
+namespace Day12;
+
+public static class Logic
+{
+    public static char START = 'S';
+    public static char END = 'E';
+
+    public static int Part1(string filepath)
+    {
+        var matrix = CreateMatrix(filepath);
+        var starterPoint = FindStartPoint(matrix);
+
+        var result = Recursive(matrix, starterPoint, Position.Undefined);
+
+        if (result.EndsWith(END))
+            return result.Length;
+        return 0;
+    }
+
+    public static string Recursive(char[][] matrix, (int x, int y) currentPosition, Position lastPosition)
+    {
+        var currentChar = matrix[currentPosition.y][currentPosition.x];
+
+        var topChar = GetTop(matrix, currentPosition);
+        var bottomChar = GetBottom(matrix, currentPosition);
+        var leftChar = GetLeft(matrix, currentPosition);
+        var rightChar = GetRight(matrix, currentPosition);
+
+        if (lastPosition != Position.Bottom && IsPossiblePath(currentChar, topChar))
+        {
+            return topChar + Recursive(matrix, (currentPosition.x, currentPosition.y - 1), Position.Top);
+        }
+
+        if (lastPosition != Position.Top && IsPossiblePath(currentChar, bottomChar))
+        {
+            return bottomChar + Recursive(matrix, (currentPosition.x, currentPosition.y + 1), Position.Bottom);
+        }
+
+        if (lastPosition != Position.Right && IsPossiblePath(currentChar, leftChar))
+        {
+            return leftChar + Recursive(matrix, (currentPosition.x - 1, currentPosition.y), Position.Left);
+        }
+
+        if (lastPosition != Position.Left && IsPossiblePath(currentChar, rightChar))
+        {
+            return rightChar + Recursive(matrix, (currentPosition.x + 1, currentPosition.y), Position.Right);
+        }
+
+        return string.Empty;
+    }
+
+    public static char[][] CreateMatrix(string filepath)
+    {
+        var lines = ReadFile.Lines(filepath).ToArray();
+        var result = new List<char[]>();
+        foreach (var line in lines)
+        {
+            result.Add(line.ToCharArray());
+        }
+
+        return result.ToArray();
+    }
+
+    public static (int x, int y) FindStartPoint(char[][] matrix)
+    {
+        for (int y = 0; y < matrix.Length; y++)
+        {
+            for (int x = 0; x < matrix[y].Length; x++)
+            {
+                if (matrix[y][x].Equals(START))
+                    return (x, y);
+            }
+        }
+
+        throw new Exception("Starter point not found");
+    }
+
+    public static bool IsPossiblePath(char currentPath, char? nextPath)
+    {
+        if (currentPath == START)
+        {
+            return nextPath.HasValue && nextPath.Value == 'a';
+        }
+
+        return nextPath.HasValue && (currentPath == nextPath || currentPath + 1 == nextPath);
+    }
+
+    public static char? GetTop(char[][] matrix, (int x, int y) position)
+    {
+        return (position.y - 1) < 0 ? null : matrix[position.y - 1][position.x];
+    }
+
+    public static char? GetBottom(char[][] matrix, (int x, int y) position)
+    {
+        return (position.y + 1 > matrix.Length - 1) ? null : matrix[position.y + 1][position.x];
+    }
+
+    public static char? GetLeft(char[][] matrix, (int x, int y) position)
+    {
+        return (position.x - 1 < 0) ? null : matrix[position.y][position.x - 1];
+    }
+
+    public static char? GetRight(char[][] matrix, (int x, int y) position)
+    {
+        return (position.x + 1 > matrix[0].Length - 1) ? null : matrix[position.y][position.x + 1];
+    }
+}
+
+public enum Position
+{
+    Undefined,
+    Top,
+    Bottom,
+    Left,
+    Right
+}
---
+++ 2022/Day12Test/LogicTest.cs
@@ -0,0 +1,102 @@
+using Day12;
+using FluentAssertions;
+
+namespace Day12Test;
+
+public class LogicTest
+{
+
+    [Test]
+    public void Part1()
+    {
+        var result = Logic.Part1(@"data\example.txt");
+        result.Should().Be(31);
+    }
+
+    [Test]
+    public void CreateMatrix()
+    {
+        var matrix = Logic.CreateMatrix(@"data\example.txt");
+        matrix.Length.Should().Be(5);
+        matrix[0].Length.Should().Be(8);
+        matrix[0][0].Should().Be(Logic.START);
+        matrix[2][5].Should().Be(Logic.END);
+    }
+
+    [Test]
+    public void FindStartPoint()
+    {
+        var matrix = new char[][]{
+            "as".ToCharArray(),
+            "cS".ToCharArray(),
+        };
+
+        var result = Logic.FindStartPoint(matrix);
+        result.Should().Be((1,1));
+    }
+
+    [TestCase('a', 'b', true)]
+    [TestCase('a', 'c', false)]
+    [TestCase('b', 'a', false)]
+    [TestCase('b', null, false)]
+    [TestCase('S', 'a', true)]
+    [TestCase('S', 'b', false)]
+    [TestCase('a', 'a', true)]
+    public void IsPossiblePath(char char1, char? char2, bool expected)
+    {
+        var result = Logic.IsPossiblePath(char1, char2);
+        result.Should().Be(expected);
+    }
+
+    [TestCase(0, 0, null)]
+    [TestCase(0, 1, 'a')]
+    public void GetTop(int x, int y, char? expected)
+    {
+        var position = (x, y);
+        var matrix = new char[][]{
+            "ab".ToCharArray(),
+            "cd".ToCharArray(),
+        };
+        var result = Logic.GetTop(matrix, position);
+        result.Should().Be(expected);
+    }
+
+    [TestCase(0, 0, 'c')]
+    [TestCase(0, 1, null)]
+    public void GetBottom(int x, int y, char? expected)
+    {
+        var position = (x, y);
+        var matrix = new char[][]{
+            "ab".ToCharArray(),
+            "cd".ToCharArray(),
+        };
+        var result = Logic.GetBottom(matrix, position);
+        result.Should().Be(expected);
+    }
+
+    [TestCase(0, 0, null)]
+    [TestCase(1, 0, 'a')]
+    public void GetLeft(int x, int y, char? expected)
+    {
+        var position = (x, y);
+        var matrix = new char[][]{
+            "ab".ToCharArray(),
+            "cd".ToCharArray(),
+        };
+        var result = Logic.GetLeft(matrix, position);
+        result.Should().Be(expected);
+    }
+
+    [TestCase(0, 0, 'b')]
+    [TestCase(1, 0, null)]
+    public void GetRight(int x, int y, char? expected)
+    {
+        var position = (x, y);
+        var matrix = new char[][]{
+            "ab".ToCharArray(),
+            "cd".ToCharArray(),
+        };
+        var result = Logic.GetRight(matrix, position);
+        result.Should().Be(expected);
+    }
+}
